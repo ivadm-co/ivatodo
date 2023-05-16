0abf81efae57048b950c07dbc9e0ac16a1e47cc2
ivadm-co <sivadeepikam2021@gmail.com>
2023-05-16 11:25:07 +0530

    created another js file and renamed *more* stuff

---
+++ src/Todo.js
@@ -0,0 +1,10 @@
+function Todo() {
+    return (
+      <div className="App">
+        <h1>Todo</h1>
+      </div>
+    );
+  }
+  
+  export default Todo;
+  
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TodoList from './todoList';
+import TodoList from './TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
